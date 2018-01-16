83d4a6fbc1e17a18004ec115f1f028232ae52a28
dev-keenability <dev@keenability.com>
2018-01-16 17:53:27 -0500

    adding styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  Button,
-  View
-} from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default class Product extends Component<{}> {
   render() {
     return (
       <View>
-        <Text>Android</Text>
+        <Text style={style.textStyle}>Android</Text>
         <Button title="Buy" />
       </View>
     );
   }
 }
+
+const style = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    padding: 20
+  }
+});
