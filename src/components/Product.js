53943b7cd53e7cd15d5530d7d5ed858bf6affdb1
dev-keenability <dev@keenability.com>
2018-01-16 17:57:21 -0500

    onPress event on button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, Text, Button, View, Alert } from 'react-native';
 
 export default class Product extends Component<{}> {
+
+  show() {
+    Alert.alert("You clicked me")
+  }
+
   render() {
     return (
       <View>
         <Text style={style.textStyle}>Android</Text>
-        <Button title="Buy" />
+        <Button title="Buy" onPress={this.show} />
       </View>
     );
   }
