97d78e809fa7a1872812f90bb64464bf2da638d1
dev-keenability <dev@keenability.com>
2018-01-17 11:29:26 -0500

    iterating through product array

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,12 +5,34 @@ import Product from './Product';
 
 export default class ProductList extends Component<{}> {
 
+  state = {
+    products: [
+      {
+        "id": 1,
+        "name": "Android"
+      },
+      {
+        "id": 2,
+        "name": "Nokia"
+      },
+      {
+        "id": 3,
+        "name": "iPhone"
+      },
+    ]
+  }
+
   render() {
+
+    var productList = this.state.products.map(function(product){
+      return (
+        <Product name={product.name} key={product.id} /> 
+      )
+    })
+
     return (
       <View>
-        <Product name = "Android" />
-        <Product name = "Nokia" />
-        <Product name = "iPhone" />
+        {productList}
       </View>
     );
   }
